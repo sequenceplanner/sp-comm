58a7e3c3a2255dbdd433df8c46ad0b72fd534ee0
Kristofer Bengtsson <kristofer@sekvensa.se>
2018-06-06 22:30:04 +0200

    Fixed build. Not using snapshot releases.

--- jvm/src/main/scala/sp/service/ServiceCommunicationSupport.scala
+++ jvm/src/main/scala/sp/service/ServiceCommunicationSupport.scala
@@ -52,3 +52,50 @@ class ServiceHandlerComm(resp: APISP.StatusResponse) extends Actor with MessageB
   def sendEvent(h: SPHeader) =
     publish(APISP.serviceStatusResponse, SPMessage.makeJson(h, serviceResponse))
 }
+
+
+
+case object Tick
+class AskActor(mess: SPMessage, requestTopic: String, replyTopic: String) extends Actor with MessageBussSupport {
+  subscribe(replyTopic)
+  publish(requestTopic, mess.toJson)
+
+  val senderHeader = mess.getHeaderAs[SPHeader]
+  if (senderHeader.isEmpty) {
+    context.parent ! APISP.SPError("Need an SPHeader")
+    self ! PoisonPill
+  }
+
+  // TODO: Handle answers from multiple services later
+  var servicesThatAnswered: Map[String, List[SPMessage]] = Map()
+
+  import scala.concurrent.duration._
+  import context.dispatcher
+  val ticker = context.system.scheduler.scheduleOnce(2 seconds, self, Tick)
+
+  override def receive = {
+    case x: String =>
+      for {
+        mess <- SPMessage.fromJson(x)
+        sendH <- senderHeader
+        h <- mess.getHeaderAs[SPHeader] if h.reqID == sendH.reqID
+      } yield {
+        val apisp = mess.getBodyAs[APISP]
+        apisp.foreach{
+          case x: APISP.SPACK =>
+            ticker.cancel()
+          case x =>
+            ticker.cancel()
+            context.parent ! x
+            self ! PoisonPill
+        }
+        if (apisp.isEmpty) {
+          context.parent ! mess
+        }
+
+      }
+    case Tick =>
+      context.parent ! APISP.SPError("No ACK or answer after 2 seconds")
+      self ! PoisonPill
+  }
+}
